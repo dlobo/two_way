bc5fff9139f0e63fefc726f5549b92e17f83a96d
Donald A. Lobo <lobo@yahoo.com>
2020-05-20 15:23:09 -0700

    lets handle all callbacks from gupshup and put them in specific controller files

---
+++ lib/two_way_web/controllers/gupshup_message_controller.ex
@@ -0,0 +1,34 @@
+defmodule TwoWayWeb.GupshupMessageController do
+  use TwoWayWeb, :controller
+
+  def handler(conn, params, msg) do
+    IO.puts(msg)
+    IO.inspect(params)
+    json conn, nil
+  end
+
+  def message(conn, params),
+    do: handler(conn, params, "message handler")
+
+  def text(conn, params),
+    do:  handler(conn, params, "text handler")
+
+  def image(conn, params),
+    do:  handler(conn, params, "image handler")
+
+  def file(conn, params),
+    do:  handler(conn, params, "file handler")
+
+  def audio(conn, params),
+    do:  handler(conn, params, "audio handler")
+
+  def video(conn, params),
+    do:  handler(conn, params, "video handler")
+
+  def contact(conn, params),
+    do:  handler(conn, params, "contact handler")
+
+  def location(conn, params),
+    do:  handler(conn, params, "location handler")
+
+end
---
+++ lib/two_way_web/controllers/gupshup_user_event_controller.ex
@@ -0,0 +1,22 @@
+defmodule TwoWayWeb.GupshupUserEventController do
+  use TwoWayWeb, :controller
+
+  def handler(conn, params, msg) do
+    IO.puts(msg)
+    IO.inspect(params)
+    json conn, nil
+  end
+
+  def user_event(conn, params),
+    do: handler(conn, params, "User event handler")
+
+  def sandbox_start(conn, params),
+    do: handler(conn, params, "Sandbox start handler")
+
+  def opted_in(conn, params),
+    do: handler(conn, params, "Opted in handler")
+
+  def opted_out(conn, params),
+    do: handler(conn, params, "Opted out handler")
+
+end
