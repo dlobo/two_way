45ad7de5306f2d571e68caa83d555dcc284eb31f
pankaj-ag <pankaj@coloredcow.in>
2020-05-21 19:05:00 +0530

    Using httpoison

--- lib/two_way/gupshup_api.ex
+++ lib/two_way/gupshup_api.ex
@@ -1,28 +1,23 @@
 defmodule TwoWay.GupshupApi do
-  @apiurl "https://api.gupshup.io/sm/api/v1/msg"
-
-  @headers [
-    {"content-type", "application/x-www-form-urlencoded"},
-    {"apikey", "380a3225dc604909c9cb8406c7d49f75"},
-    {"Cache-Control", "no-cache"}
-  ]
-
-  @request_body %{:channel => :whatsapp}
-
-
   def make_call(payload, destination) do
-    headers = @headers
 
     request_body =
-      @request_body
+      %{"channel" => "whatsapp"}
       |> Map.merge(get_source)
       |> Map.put(:destination, destination)
-      |> Map.put("message.payload", payload)
+      |> Map.put("message", "Hello")
+      |> Poison.encode!
 
-    case Mojito.request(:post, @apiurl, @headers, URI.encode_query(request_body)) do
-      {:ok, response} -> success_response(response)
-      {:error, error} -> failed_response(error)
-    end
+    url = System.get_env("GUPSHUP_API_URL") || "https://api.gupshup.io/sm/api/v1/msg"
+    response = HTTPoison.post(url, request_body, get_headers())
+    IO.inspect(response)
+
+    # case Mojito.post(url, get_headers(), Jason.encode!(request_body),
+    #        opts: [transport_opts: [verify: :verify_none]]
+    #      ) do
+    #   {:ok, response} -> success_response(response)
+    #   {:error, error} -> failed_response(error)
+    # end
   end
 
   def success_response(response) do
@@ -30,42 +25,50 @@ defmodule TwoWay.GupshupApi do
   end
 
   def failed_response(error) do
-    IO.inspect(response)
+    IO.inspect(error)
+  end
+
+  def message(payload, destination) do
+    # validate_payload(message, "message")
+    make_call(payload, destination)
+  end
+
+  def image(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def file(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def video(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def audio(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def template(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def location(payload, destination) do
+    make_call(payload, destination)
+  end
+
+  def contact(payload, destination) do
+    make_call(payload, destination)
   end
 
   def get_source() do
-    %{:source => "917834811114", "src.name" => "NGO Name"}
+    %{"source" => "917834811114", "src.name" => "NGO Name"}
   end
 
-  def message(message, destination) do
-    make_call(message, destination)
-  end
-
-  def image(message, destination) do
-    make_call(message, destination)
-  end
-
-  def file(message, destination) do
-    make_call(message, destination)
-  end
-
-  def video(message, destination) do
-    make_call(message, destination)
-  end
-
-  def audio(message, destination) do
-    make_call(message, destination)
-  end
-
-  def template(message, destination) do
-    make_call(message, destination)
-  end
-
-  def location(message, destination) do
-    make_call(message, destination)
-  end
-
-  def contact(message, destination) do
-    make_call(message, destination)
+  def get_headers() do
+    [
+      {"content-type", "application/x-www-form-urlencoded"},
+      {"apikey", System.get_env("GUPSHUP_API_KEY") || "380a3225dc604909c9cb8406c7d49f75"}
+    ]
   end
 end
