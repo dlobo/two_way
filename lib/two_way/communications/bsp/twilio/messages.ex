82efcbe374137b834c72b3dee545818d4d02c0eb
pankaj-ag <pankaj@coloredcow.in>
2020-05-27 14:11:55 +0530

    Remove warnings

--- lib/two_way/communications/bsp/twilio/messages.ex
+++ lib/two_way/communications/bsp/twilio/messages.ex
@@ -1,9 +1,6 @@
 defmodule TwoWay.Commnunication.BSP.Twilio.Message do
 
-  @channel "whatsapp"
   @behaviour TwoWay.Communication.MessageBehaviour
-
-  alias TwoWay.Commnunication.BSP.Twilio.ApiClient, as: ApiClient
 
   @impl TwoWay.Communication.MessageBehaviour
   def send_text(_payload, _receiver, _sender), do: {:ok, "Message sent via twilio"}
