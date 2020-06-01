110015b5e84f3307bea03da78177f95fa4107c07
pankaj-ag <pankaj@coloredcow.in>
2020-06-01 16:49:21 +0530

    Error handling for outbound message

--- lib/two_way/communications/bsp/gupshup/worker.ex
+++ lib/two_way/communications/bsp/gupshup/worker.ex
@@ -7,30 +7,38 @@ defmodule TwoWay.Communications.BSP.Gupshup.Worker do
   alias TwoWay.Communications.BSP.Gupshup.ApiClient
   alias TwoWay.{Messages, Messages.Message}
 
-  @rate_name  Application.fetch_env!(:two_way, :bsp_id)
+  @rate_name Application.fetch_env!(:two_way, :bsp_id)
   @rate_limit Application.fetch_env!(:two_way, :bsp_limit)
 
   @impl Oban.Worker
   def perform(%{"message" => message, "payload" => payload}, _job) do
     # ensure that we are under the rate limit, all rate limits are in requests/minutes
     with {:ok, _} <- ExRated.check_rate(@rate_name, 60_000, @rate_limit) do
-      response =
         ApiClient.post("/msg", payload)
-        |> handle_response()
-
-      Poison.Decode.decode(message, as: %Message{})
-      |> Messages.update_message(%{wa_message_id: response.message_id, wa_status: :enqueued})
-
-      :ok
+        |> handle_response(message)
     else
       _ -> {:error, :rate_limit_exceeded}
     end
   end
 
-  defp handle_response(response_data) do
-    {:ok, response} = response_data
+  defp handle_response({:ok, response}, message) do
+    case response do
+      %Tesla.Env{status: 200} -> success_response(response, message)
+      _ -> error_response(response, message)
+    end
+  end
+
+  defp success_response(response, message) do
     body = response.body |> Jason.decode!()
-    %{message_id: body["messageId"]}
+
+    Poison.Decode.decode(message, as: %Message{})
+    |> Messages.update_message(%{wa_message_id: body["messageId"], wa_status: :enqueued})
+
+    {:ok, message}
+  end
+
+  defp error_response(response, _message) do
+    {:error, response.body}
   end
 
   @impl Oban.Worker
@@ -39,5 +47,4 @@ defmodule TwoWay.Communications.BSP.Gupshup.Worker do
     # this needs more work and tweaking
     :math.pow(attempt, 4) + 60 + :rand.uniform(30) * attempt
   end
-
 end
