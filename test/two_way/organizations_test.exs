a3756ca5c9e195a91867a335fe7144f07f6d7e0d
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-01 16:21:37 +0530

    BSP: migration and schema

--- test/two_way/organizations_test.exs
+++ test/two_way/organizations_test.exs
@@ -100,4 +100,67 @@ defmodule TwoWay.OrganizationsTest do
       assert %Ecto.Changeset{} = Organizations.change_organization(organization)
     end
   end
+
+  describe "bsps" do
+    alias TwoWay.Organizations.BSP
+
+    @valid_attrs %{api_end_point: "some api_end_point", name: "some name", url: "some url"}
+    @update_attrs %{api_end_point: "some updated api_end_point", name: "some updated name", url: "some updated url"}
+    @invalid_attrs %{api_end_point: nil, name: nil, url: nil}
+
+    def bsp_fixture(attrs \\ %{}) do
+      {:ok, bsp} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Organizations.create_bsp()
+
+      bsp
+    end
+
+    test "list_bsps/0 returns all bsps" do
+      bsp = bsp_fixture()
+      assert Organizations.list_bsps() == [bsp]
+    end
+
+    test "get_bsp!/1 returns the bsp with given id" do
+      bsp = bsp_fixture()
+      assert Organizations.get_bsp!(bsp.id) == bsp
+    end
+
+    test "create_bsp/1 with valid data creates a bsp" do
+      assert {:ok, %BSP{} = bsp} = Organizations.create_bsp(@valid_attrs)
+      assert bsp.api_end_point == "some api_end_point"
+      assert bsp.name == "some name"
+      assert bsp.url == "some url"
+    end
+
+    test "create_bsp/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Organizations.create_bsp(@invalid_attrs)
+    end
+
+    test "update_bsp/2 with valid data updates the bsp" do
+      bsp = bsp_fixture()
+      assert {:ok, %BSP{} = bsp} = Organizations.update_bsp(bsp, @update_attrs)
+      assert bsp.api_end_point == "some updated api_end_point"
+      assert bsp.name == "some updated name"
+      assert bsp.url == "some updated url"
+    end
+
+    test "update_bsp/2 with invalid data returns error changeset" do
+      bsp = bsp_fixture()
+      assert {:error, %Ecto.Changeset{}} = Organizations.update_bsp(bsp, @invalid_attrs)
+      assert bsp == Organizations.get_bsp!(bsp.id)
+    end
+
+    test "delete_bsp/1 deletes the bsp" do
+      bsp = bsp_fixture()
+      assert {:ok, %BSP{}} = Organizations.delete_bsp(bsp)
+      assert_raise Ecto.NoResultsError, fn -> Organizations.get_bsp!(bsp.id) end
+    end
+
+    test "change_bsp/1 returns a bsp changeset" do
+      bsp = bsp_fixture()
+      assert %Ecto.Changeset{} = Organizations.change_bsp(bsp)
+    end
+  end
 end
